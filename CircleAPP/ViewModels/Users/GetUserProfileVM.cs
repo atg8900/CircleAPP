651c262f432c653aaf72888953b09554606066a8
Mostafa <hussinym2@gmail.com>
2026-04-23 10:04:06 +0200

    User Profile display and manage details

---
+++ CircleAPP/ViewModels/Users/GetUserProfileVM.cs
@@ -0,0 +1,11 @@
+﻿using CircleApp.Data.Models;
+using CircleAPP.Models;
+
+namespace CircleAPP.ViewModels.Users
+{
+    public class GetUserProfileVM
+    {
+        public User User { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
